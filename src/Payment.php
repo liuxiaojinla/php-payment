216733c193eeeb7ea6d8b1ed32e81f27f2fc0280
晋 <1540175452@qq.com>
2019-08-06 19:50:07 +0800

    增加新的驱动

--- src/Payment.php
+++ src/Payment.php
@@ -15,6 +15,12 @@ use RuntimeException;
  * 支付API工厂
  * @method string getAppId() 获取AppId
  * @method \xin\payment\driver\Basic basic(array $options) static 获取默认的聚合支付实例
+ * @method UnifiedOrderResult unifiedOrder(UnifiedOrderOptions $input) static 统一下单
+ * @method OrderQueryResult orderQuery(OrderQueryOptions $input) static 查询订单
+ * @method CloseOrderResult closeOrder(CloseOrderOptions $input) static 关闭订单
+ * @method RefundResult refund(RefundOptions $input) static 申请退款
+ * @method RefundQueryResult refundQuery(RefundQueryOptions $input) static 退款查询
+ * @method ReverseResult reverse(ReverseOptions $input) static 撤销订单
  *
  * @package xin\payment
  */
@@ -28,6 +34,20 @@ class Payment{
 	 * @return mixed
 	 */
 	public static function __callStatic($name, $arguments){
+		if(in_array($name, [
+			'unifiedOrder',
+			'orderQuery',
+			'closeOrder',
+			'refund',
+			'refundQuery',
+			'reverse',
+		])){
+			$options = array_shift($arguments);
+			return call_user_func_array([
+				self::factory($name, $options),
+				$name,
+			], $arguments);
+		}
 		return self::factory($name, $arguments[0]);
 		//		if(strpos($name, "factory") === 0){
 		//			$driver = substr($name, 7);
